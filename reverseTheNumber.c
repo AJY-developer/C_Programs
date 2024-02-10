0e85369189d545e6964068f6a27d52fa314e054b
Ajay <ajshakya7860@gmail.com>
2024-02-10 20:29:05 +0530

    Update reverseTheNumber.c

--- reverseTheNumber.c
+++ reverseTheNumber.c
@@ -2,18 +2,17 @@
 #include<math.h>
 //#include<conio.h> // in case of old editor
 int main(void) {
-int i,num,sum=0,digit[50],weig,rev=0;
+int i,num,sum=0,digit,weig,rev=0;
   // clrscr(); // in case of old editor
   printf("enter the number: ");
   scanf("%d",&num);
   
-  weig=floor(log10(num))+1; // finding the number of digits
   
-  for(i=1;i<=weig;i++){
-     digit[i]=num%10; // separating the digits
-     num =(num-digit[i])/10;
-     sum += digit[i];
-     rev+= digit[i]*pow(10,weig-i);
+  while(num!=0){
+     digit=num%10; // separating the digits
+     num /=10;
+     sum += digit;
+     rev = rev*10+digit;
   }
 
   printf("\nnumber in reverse order=%d,\nsum of digit=%d",rev,sum);
